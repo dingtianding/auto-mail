63854188033bc5012ebcd48e9509eae26ae7c52a
Dean <dding66@gatech.edu>
2025-04-09 22:46:22 -0400

    Update build script

--- bin/build.sh
+++ bin/build.sh
@@ -2,7 +2,10 @@
 # exit on error
 set -o errexit
 
+# Add platform and install bundle
+bundle lock --add-platform x86_64-linux
 bundle install
+
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate 
